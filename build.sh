5da5115f03e0d13bdc15c979a77ec09affecb08a
Charlie <charlie@charlie-ubuntu.(none)>
2010-01-12 16:49:42 +1100

    OS is bootable - also fixes to build.sh

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 
 echo "Kernel Compilation:"
 echo "    -   Assembling bootloader..."
-nasm -i "kernel_src/" -f bin -o kernelbin/loader.o kernelsrc/loader.asm
+nasm -i "kernelsrc/" -f elf -o kernelbin/loader.o kernelsrc/loader.asm
 
 echo "    -   Compiling kernel..."
 gcc -o kernelbin/kernel.o -c kernelsrc/kernel.c -nostdlib -nostartfiles -nodefaultlibs #-masm=intel
 
 echo "    -   Linking..."
 #ld -T kernel_src/linker.ld -o kernel_bin/os.bin kernel_bin/loader.o kernel_bin/kernel.o
-ld -o kernelbin/os.bin kernelbin/loader.o kernelbin/kernel.o
+ld -T kernelsrc/linker.ld -o kernelbin/os.bin kernelbin/loader.o kernelbin/kernel.o
 
 echo "    -   Copying to build/boot/fc_krnl"
-cp kernelbin/kernel.o build/boot/fc_krnl
+cp kernelbin/os.bin build/boot/fc_krnl
 
 echo "Creating bootdisk image:"
 echo "    -   Creating floppy image"
