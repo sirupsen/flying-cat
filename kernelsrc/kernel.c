65e0ea97e8ddd1ad3632dbe9440bdd04de2722be
Charlie <charlie@charlie-ubuntu.(none)>
2010-01-14 17:03:34 +1100

    changed the hello world in kmain() to use the OS_NAME constant, rather than hardcoded OS name

--- kernelsrc/kernel.c
+++ kernelsrc/kernel.c
@@ -28,9 +28,10 @@ void kmain(multiboot_info_t* mbi, unsigned int magic)
  
 	// we booted successfully, so say hello world and dump some info
 	println("Hello, World!", 0);
-	println("This is Flying Cat (c) Turbsen 2010", 1);
+	println("This is " OS_NAME " (c) Turbsen 2010", 1);
 	println((char*)mbi->boot_loader_name, 3);
 	println((char*)mbi->cmdline, 4);
+	
  
 	for(;;); // hang
 }
