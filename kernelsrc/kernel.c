27cc837d56d754c9cbde265829b4481e5c8f406e
Charlie <charlie@charlie-ubuntu.(none)>
2010-01-14 17:05:08 +1100

    empty kernel.lua file

--- kernelsrc/kernel.c
+++ kernelsrc/kernel.c
@@ -31,7 +31,6 @@ void kmain(multiboot_info_t* mbi, unsigned int magic)
 	println("This is " OS_NAME " (c) Turbsen 2010", 1);
 	println((char*)mbi->boot_loader_name, 3);
 	println((char*)mbi->cmdline, 4);
-	
  
 	for(;;); // hang
 }
