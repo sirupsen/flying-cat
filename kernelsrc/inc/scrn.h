53045fc78e626d6733e71385a88c305060b75b6d
Simon <sirup@sirupsen.dk>
2010-01-16 13:55:14 +0100

    Coding style

--- kernelsrc/inc/scrn.h
+++ kernelsrc/inc/scrn.h
@@ -1,27 +1,28 @@
-k_clear_screen() {
-  char* vram = (char*)(0xb8000+(80*25*2) - 1);
-  while (vram >= (char*)0xb8000) {
-    *vram-- = 0;
-  }
+void k_clear_screen() 
+{
+	char* vram = (char*)(0xb8000+(80*25*2) - 1);
+	while (vram >= (char*)0xb8000)
+		*vram-- = 0;
 }
 
-k_print(char *message) {
-  // Line number by default is 0
-  static int line = 0;
-  // @todo I don't want to know what happens if the lins is > 24, make scroll stuff
-  char* vram = (char*) (0xb8000 + (80 * line * 2));
+void k_print(char *message) 
+{
+	// Line number by default is 0
+	static int line = 0;
+	// @todo I don't want to know what happens if the lins is > 24, make scroll stuff
+	char* vram = (char*) (0xb8000 + (80 * line * 2));
 
-  while(*message) {
-    // If newline
-    if (*message == '\n') {
-      line++;
-      *message++;
-    // Not newline
-    } else {
-      *vram++ = *message++;
-      *vram++ = 0x07;
-    }
-  }
+	while(*message) {
+		// If newline
+		if (*message == '\n') {
+			line++;
+			*message++;
+			// Not newline
+		} else {
+			*vram++ = *message++;
+			*vram++ = 0x07;
+		}
+	}
 
-  line++;
+	line++;
 }
--- kernelsrc/inc/string.h
+++ kernelsrc/inc/string.h
@@ -1,36 +1,36 @@
 int strlen(const char *str)
 {
-  unsigned int length;
+	unsigned int length;
 
-  // For each character until \0, plus length with one
-  for (length = 0; *str != '\0'; str++) length++;
-  // Return this length
-  return length;
+	// For each character until \0, plus length with one
+	for (length = 0; *str != '\0'; str++) length++;
+	// Return this length
+	return length;
 }
 
 char *strcpy(char *destination, const char *source) 
 {
-  unsigned int i;
+	unsigned int i;
 
-  /*
-   * Loop over source until end (\0), and put everything
-   * into destination.
-   */
-  for (i = 0; source[i] != '\0'; i++)
-    destination[i] = source[i];
+	/*
+	 * Loop over source until end (\0), and put everything
+	 * into destination.
+	 */
+	for (i = 0; source[i] != '\0'; i++)
+		destination[i] = source[i];
 
-  // Append \0 to destination 
-  destination[i] = '\0';
+	// Append \0 to destination 
+	destination[i] = '\0';
 
-  // Return the destination
-  return destination;
+	// Return the destination
+	return destination;
 }
 
 char *strcat (char *destination, const char *source)
 {
-  // Let's just use these functions which we already defined
-  strcpy(destination + strlen(destination), source);
+	// Let's just use these functions which we already defined
+	strcpy(destination + strlen(destination), source);
 
-  // Then return the destination
-  return destination;
+	// Then return the destination
+	return destination;
 }
