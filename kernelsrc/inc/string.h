29230705d3a0a636378debf7ada82dcf62985034
Simon <sirup@sirupsen.dk>
2010-01-16 12:52:59 +0100

    Strlen comments, strcpy

--- kernelsrc/inc/string.h
+++ kernelsrc/inc/string.h
@@ -1,6 +1,27 @@
 int strlen(const char *str)
 {
-    int retval;
-    for(retval = 0; *str != '\0'; str++) retval++;
-    return retval;
+  unsigned int length;
+
+  // For each character until \0, plus length with one
+  for (lenght = 0; *str != '\0'; str++) length++;
+  // Return this length
+  return length;
+}
+
+char *strcpy(char *destination, const char *source) 
+{
+  unsigned int i;
+
+  /*
+   * Loop over source until end (\0), and put everything
+   * into destination.
+   */
+  for (i = 0; source[i] != '\0'; i++)
+    destination[i] = source[i];
+
+  // Append \0 to destination 
+  destination[i] = '\0';
+
+  // Return the destination
+  return destination;
 }
